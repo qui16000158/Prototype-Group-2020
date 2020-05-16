9e9218102dc3553c4a2105053af11b2e07e8d714
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2020-05-16 11:52:28 +0100

    Added a single comment.

--- Assets/Scripts/NPCInfo.cs
+++ Assets/Scripts/NPCInfo.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // James Quinney - QUI16000158
-// 
+// This holds basic information about
+// NPCs, such as their name and the
+// conversation that loads when talking
+// to them
 public class NPCInfo : MonoBehaviour
 {
     public new string name = "NPC"; // The NPCs name (Can be set in inspector)
