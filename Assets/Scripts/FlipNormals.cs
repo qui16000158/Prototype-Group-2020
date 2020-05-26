7ca587d2047adae0ad2a39001ff0a19c9e616d40
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2020-05-26 12:46:41 +0100

    Added Collision Mesh in Space
    This will stop the player from leaving the map selection area.

---
+++ Assets/Scripts/FlipNormals.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// James Quinney - QUI16000158
+// This will flip the normals of a mesh
+// and then add a mesh collider
+public class FlipNormals : MonoBehaviour
+{
+    [SerializeField]
+    MeshFilter mf; // The object's mesh filter
+    Mesh mesh; // The object's mesh
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = mf.mesh; // Grab the mesh from the mesh filter
+
+        int[] triangles = new int[mesh.triangles.Length]; // Create an array to hold new triangle indices
+        // Jump through the array in threes, because triangles have 3 points
+        for(int i = 0;i<triangles.Length;i+=3){
+            // Flip the 1st, and third point of each triangle
+            triangles[i] = mesh.triangles[i+2];
+            triangles[i+2] = mesh.triangles[i];
+
+            // Maintail 2nd point's index
+            triangles[i+1] = mesh.triangles[i+1];
+        }
+
+        mesh.triangles = triangles; // Override triangles in the mesh
+
+        gameObject.AddComponent<MeshCollider>(); // Add a mesh collider
+    }
+}
---
+++ Assets/Scripts/LookTowards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// James Quinney - QUI16000158
+// This script will make one object
+// look towards another on the Y-axis
+public class LookTowards : MonoBehaviour
+{
+    [SerializeField]
+    Transform lookAt; // The object to look at
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(lookAt); // Make the object look at the other object
+        transform.eulerAngles = new Vector3(0f, transform.eulerAngles.y, 0f); // Reset X, and Z rotation
+    }
+}
