b899e6f8664bac0b00faeba116d8fa328c891c6c
qui16000158 <55698561+qui16000158@users.noreply.github.com>
2020-06-04 11:19:00 +0100

    Fixed audio sliders not persisting between scenes

--- Assets/Scripts/Misc/AudioVolume.cs
+++ Assets/Scripts/Misc/AudioVolume.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 // James Quinney - QUI16000158
 // Will allow us to logarithmically
@@ -10,9 +11,21 @@ public class AudioVolume : MonoBehaviour
 {
     [SerializeField]
     AudioMixer mixer; // The audio mixer
+    static Dictionary<AudioMixer, float> sliderValues = new Dictionary<AudioMixer, float>(); // Cached values for sliders
+
+    // Runs before first frame update
+    private void Start()
+    {
+        // Check if a slider value has been stored
+        if (sliderValues.ContainsKey(mixer))
+        {
+            GetComponent<Slider>().value = sliderValues[mixer]; // Restore slider value
+        }
+    }
 
     // Update the volume
     public void UpdateVolume(float level){
+        sliderValues[mixer] = level; // Store the current value
         mixer.SetFloat("Volume", Mathf.Log10(level) * 20f); // Set the volume level logarithmically
     }
 }
